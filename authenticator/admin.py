a9a90ee2a0182202f3c8a3cc3260a4fc72985873
Santiago Davila <davilag.sj@gmail.com>
2020-07-07 14:02:41 -0300

    admin interface activated with the new table 'authenticator_personal_data'

--- authenticator/admin.py
+++ authenticator/admin.py
@@ -1,3 +1,18 @@
+#Django
 from django.contrib import admin
+
+#Local
+from authenticator.models import Personal_data
+
+@admin.register(Personal_data)
+class authenticator_admin(admin.ModelAdmin):
+
+    list_display = ('pk', 'user', 'nationality', 'birth_date')
+    list_display_links = ('pk', 'user')
+    list_editable = ('birth_date', )
+    search_fields = ('user__email', 'user__first_name', 'user__last_name', )
+    list_filter = ('created', 'modified')
+    
+
 
 # Register your models here.
