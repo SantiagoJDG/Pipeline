eeb34ebff500f071314b2eb2f7dc9c0e35d5b9e5
Santiago Davila <davilag.sj@gmail.com>
2020-07-08 14:00:45 -0300

    Log_in.py view creation

--- authenticator/views.py
+++ authenticator/views.py
@@ -1,3 +1,19 @@
+#Django
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.backends import BaseBackend
 
-# Create your views here.
+
+def log_in(request):
+
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not none:
+        login(request, user)
+        return f'Succesful login'
+    else: 
+        return f'Unsuccesfrul login, please try again'
+
+
