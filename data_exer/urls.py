63d9a1abb2394509afec1711096e37bc33c290ef
Santiago Davila <davilag.sj@gmail.com>
2020-08-21 21:50:37 -0300

    Instaling settings for tests app

--- data_exer/urls.py
+++ data_exer/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from authenticator import views as auth_views
+from e10_tests import views as e10_views 
 
 
 urlpatterns = [
@@ -10,5 +11,7 @@ urlpatterns = [
     path('auth/login/user_creation/', auth_views.create_users, name='user_creation'),
     path('auth/usersland/', auth_views.CD_form_view, name='data_completion'),
     path('logout/', auth_views.logout_view, name='logout'),
+
+    path('tests/', e10_views.tests_view, name='tests')
 
 ]
--- e10_tests/apps.py
+++ e10_tests/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class E10TestsConfig(AppConfig):
     name = 'e10_tests'
+    verbose_name = 'e10_tests'
